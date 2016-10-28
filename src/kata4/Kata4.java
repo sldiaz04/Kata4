2012c506bee29ab1b44898a0f31ebfd365ba178c
Sergio <sldiaz04@gmail.com>
2016-10-28 12:15:00 +0100

    Kata4 v3

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,20 +1,16 @@
 package kata4;
 
-import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Kata4 {
     
-    public static void main(String [] args){
-        File file = new File("c:\\");
-        print(file.listFiles(),"");
-    }
-    
-    public static void print(File [] files,String indent){
-        if(files == null) return;
-        for (File file : files) {
-            System.out.println(indent + (file.isDirectory() ? "+" : "-") + file.getName());
-            if(!file.isDirectory() || file.isHidden()) continue;
-            print(file.listFiles()," ");
-        }
+    public static void main(String [] args) throws IOException{
+        String fileName = "D:\\DOCUMENTOS\\MIO\\NetBeansProjects\\Kata4\\emailsfilev1.txt";
+        ArrayList<String> mailList = MailListReader.read(fileName);
+        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        HistogramDisplay histogramDisplay = new HistogramDisplay(histogram);
+        histogramDisplay.execute();
+        
     }
 }
